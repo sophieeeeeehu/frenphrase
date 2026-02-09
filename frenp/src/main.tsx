ff0c88dbfa5be1298ecd7b4a6d0975378886209d
Sophie Hu <113879830+sophieeeeeehu@users.noreply.github.com>
2026-02-08 20:36:57 -0800

    fix import problems

--- frenp/src/main.tsx
+++ frenp/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Exemples from './exemples.tsx'
+// import Exemples from './exemples.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
